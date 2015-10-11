d5875059f06b3c3bb524107ab74ab870678acd25
Masato Ikeda <masato.ikeda@gmail.com>
2015-10-11 20:00:21 +0900

    Reduce width, but failed...

--- widget.js
+++ widget.js
@@ -1,4 +1,6 @@
 var webview = document.getElementById("widget");
 webview.addEventListener("did-finish-load", function() {
-  webview.insertCSS("#area-game { width: 900px }");
+  webview.insertCSS("#html, #body, #w, #main-ntg, #area-game { width: 900px }");
+  webview.insertCSS("#body { min-width: 900px }");
+  webview.insertCSS(".ntgnavi-r, #ntg-recommend, .area-naviapp, #foot { display: none; }");
 });
