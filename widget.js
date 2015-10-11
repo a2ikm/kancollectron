475cf47a3b53bc730cb80fea97492cae8d17d323
Masato Ikeda <masato.ikeda@gmail.com>
2015-10-12 01:27:18 +0900

    Mute from tool menu

--- widget.js
+++ widget.js
@@ -4,3 +4,32 @@ webview.addEventListener("did-finish-load", function() {
   webview.insertCSS("#body { min-width: 900px }");
   webview.insertCSS(".ntgnavi-r, #ntg-recommend, .area-naviapp, #foot { display: none; }");
 });
+
+var remote = require("remote");
+var Menu = remote.require("menu");
+var menu = Menu.buildFromTemplate([
+  {
+    label: 'kancollectron',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click: function() { remote.require('app').quit(); }
+      },
+    ]
+  },
+  {
+    label: 'Tool',
+    submenu: [
+      {
+        label: 'Toggle Audio Muted',
+        accelerator: 'Command+A',
+        click: function() {
+          var muted = webview.isAudioMuted();
+          webview.setAudioMuted(!muted);
+        }
+      }
+    ]
+  }
+]);
+Menu.setApplicationMenu(menu);
