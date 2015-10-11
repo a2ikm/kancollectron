ac72ec341bc397113db7451a1d928091c8396583
Masato Ikeda <masato.ikeda@gmail.com>
2015-10-12 02:20:23 +0900

    Capture screen

--- widget.js
+++ widget.js
@@ -5,6 +5,11 @@ webview.addEventListener("did-finish-load", function() {
   webview.insertCSS(".ntgnavi-r, #ntg-recommend, .area-naviapp, #foot { display: none; }");
 });
 
+var path = require("path");
+var homeDir = require("home-dir");
+var strftime = require("strftime");
+var mkdirp = require("mkdirp");
+
 var remote = require("remote");
 var Menu = remote.require("menu");
 var menu = Menu.buildFromTemplate([
@@ -27,6 +32,23 @@ var menu = Menu.buildFromTemplate([
         click: function() {
           var muted = webview.isAudioMuted();
           webview.setAudioMuted(!muted);
+        }
+      },
+      {
+        label: 'Capture Screen',
+        accelerator: 'Command+C',
+        click: function(item, focusedWindow) {
+          focusedWindow.capturePage(function(image) {
+            var dir = path.join(homeDir(), "Documents", "kancollectron");
+            mkdirp(dir, function(err) {
+              if (err) {
+                alert(err);
+              } else {
+                var out = path.join(dir, strftime("%Y%m%d%H%M%S.png"));
+                remote.require("fs").writeFile(out, image.toPng());
+              }
+            });
+          });
         }
       }
     ]
